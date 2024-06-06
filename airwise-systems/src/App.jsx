1ff20d459d99f05032c8a801eadb30bd0f4c3ebe
slaughtbear <ivanaguirre368@gmail.com>
2024-06-05 18:55:05 -0600

    Instalar NextUI

--- airwise-systems/src/App.jsx
+++ airwise-systems/src/App.jsx
@@ -1,9 +1,10 @@
+import Card from './components/Card'
 import './App.css'
 
 function App() {
   return (
     <>
-      <button className='bg-purple-600 text-white rounded cursor-pointer py-3 px-6'>Count</button>
+    <Card />
     </>
   )
 }
---
+++ airwise-systems/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import {Input} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <Input
+      isRequired
+      type="email"
+      label="Email"
+      defaultValue="junior@nextui.org"
+      className="max-w-xs"
+    />
+  );
+}
