c78a5c472cd60deebfa521515063d792aedceef9
slaughtbear <ivanaguirre368@gmail.com>
2024-06-17 18:42:02 -0600

    Update:(Banner) Diseño resposive + Fuentes nuevas

--- airwise-systems/src/interface/banner/Banner.jsx
+++ airwise-systems/src/interface/banner/Banner.jsx
@@ -4,27 +4,27 @@ import bannerCover from "./svg/bannerCover.svg";
 
 function Banner() {
   return (
-    <div className="relative flex items-center justify-center overflow-hidden h-screen bg-white dark:bg-gray-800">
+    <div className="relative flex items-center justify-center overflow-hidden min-h-screen">
       <div className="absolute inset-0 z-10">
         <div className="absolute top-0 left-0 w-full h-full z-10 bg-black opacity-80 backdrop-filter backdrop-blur-sm"></div>
         <Lumiflex />
       </div>
 
       <div className="container relative z-20 flex flex-col items-center justify-center px-6 py-16 mx-auto lg:flex-row lg:space-x-0">
-        <div className="relative z-20 flex flex-col items-center justify-center text-center w-full lg:w-1/2 lg:items-center lg:text-center">
-          <h1 className="flex flex-col text-6xl font-black leading-none text-purple-600 uppercase font-bebas-neue sm:text-8xl dark:text-white">
+        <div className="relative z-20 flex flex-col items-center justify-center text-center w-full lg:w-1/2 lg:items-start lg:text-left">
+          <h1 className="flex flex-col text-5xl font-black leading-none text-purple-600 uppercase kanit-black sm:text-6xl md:text-7xl lg:text-8xl">
             Air-Wise
-            <span className="text-5xl sm:text-7xl">Systems</span>
+            <span className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl">Systems</span>
           </h1>
-          <p className="text-sm mt-4 text-white sm:text-base dark:text-white">
+          <p className="text-sm mt-4 text-white sm:text-base work-sans-aesthetic">
             Nuestro sistema analiza y reporta la calidad del aire, detectando
             gases peligrosos y otras impurezas. Asegura la salud y bienestar de
             tu equipo con datos en tiempo real y respuestas rápidas a cualquier
             riesgo ambiental.
           </p>
-          <div className="flex mt-4 justify-center">
+          <div className="flex mt-4 justify-center lg:justify-start">
             <Button
-              className="px-4 py-2 mr-4 text-purple-300"
+              className="px-4 py-2 mr-4 text-purple-300 work-sans-aesthetic"
               color="secondary"
               variant="flat"
               size="lg"
@@ -32,7 +32,7 @@ function Banner() {
               COMENZAR AHORA
             </Button>
             <Button
-              className="px-4 py-2 text-purple-300"
+              className="px-4 py-2 text-purple-300 work-sans-aesthetic"
               color="secondary"
               variant="ghost"
               size="lg"
--- airwise-systems/src/interface/home/Home.jsx
+++ airwise-systems/src/interface/home/Home.jsx
@@ -4,7 +4,7 @@ import Services from "../services/Services";
 import About from "../about/About";
 import Footer from "../footer/Footer";
 //import Register from "../register/Register";
-import Login from "../login/Login";
+//import Login from "../login/Login";
 
 function Home() {
   return (
@@ -15,7 +15,6 @@ function Home() {
         <About />
         <Services />
         <Footer />
-        <Login />
       </div>
     </div>
   );
