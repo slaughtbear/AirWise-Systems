d2df472c50129c3c0a3f414b01bf7e33c6281094
slaughtbear <ivanaguirre368@gmail.com>
2024-07-23 14:03:45 -0600

    Update: Se eliminaron elementos, y se agregaron Avatars para los usuarios

--- airwise-systems/src/interface/dashboard/Navbar/NavDashboard.jsx
+++ airwise-systems/src/interface/dashboard/Navbar/NavDashboard.jsx
@@ -1,28 +1,21 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { GrMagic } from "react-icons/gr";
 import {
   Navbar as NextUINavbar,
   NavbarBrand,
-  NavbarMenuToggle,
-  NavbarMenuItem,
-  NavbarMenu,
   NavbarContent,
-  NavbarItem,
-  Link,
   DropdownItem,
   DropdownTrigger,
   Dropdown,
   DropdownMenu,
-  Avatar,
 } from "@nextui-org/react";
 import { signOut } from "firebase/auth";
+import Avvvatars from "avvvatars-react";
 import { auth } from "../../../firebase/FirebaseConfig";
 import { useNavigate } from "react-router-dom";
 
 function NavDashboard() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const [username, setUsername] = useState(""); // Estado para almacenar el nombre de usuario
-  const menuItems = ["Acerca de", "Servicios", "Contacto"];
   const navigate = useNavigate(); // Crear una instancia de useNavigate
 
   const handleLogout = async () => {
@@ -34,29 +27,17 @@ function NavDashboard() {
     }
   };
 
-    // Obtener el nombre de usuario al montar el componente
-    useEffect(() => {
-      const user = auth.currentUser;
-      if (user) {
-        setUsername(user.displayName || user.email); // Asignar el nombre de usuario o correo electrónico si no hay nombre
-      }
-    }, []);
+  // Obtener el nombre de usuario al montar el componente
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      setUsername(user.displayName || user.email); // Asignar el nombre de usuario o correo electrónico si no hay nombre
+    }
+  }, []);
 
   return (
-    <NextUINavbar
-      onMenuOpenChange={setIsMenuOpen}
-      isBordered
-      className="work-sans-aesthetic"
-      style={{
-        backgroundColor: "rgba(0, 0, 0, 0.800)",
-        backdropFilter: "blur(6px)",
-      }}
-    >
+    <NextUINavbar>
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden text-white"
-        />
         <NavbarBrand>
           <GrMagic className="text-purple-500" />
           <p className="font-bold kanit-black text-inherit pl-3 text-white">
@@ -64,36 +45,12 @@ function NavDashboard() {
           </p>
         </NavbarBrand>
       </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link className="text-white" href="#">
-            Acerca de
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link className="text-purple-500" href="#" aria-current="page">
-            Servicios
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link className="text-white" href="#">
-            Contacto
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent as="div" justify="end">
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
+            <button className="transition-transform focus:outline-none">
+              <Avvvatars value={username} size={40} />
+            </button>
           </DropdownTrigger>
           <DropdownMenu
             aria-label="Profile Actions"
@@ -118,20 +75,6 @@ function NavDashboard() {
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
-      <NavbarMenu
-        style={{
-          backgroundColor: "rgba(0, 0, 0, 0.750)",
-          backdropFilter: "blur(5px)",
-        }}
-      >
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link className="w-full text-white" href="#" size="lg">
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
     </NextUINavbar>
   );
 }
