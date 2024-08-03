07e7d6e1dfb57b71afa36e0c019906867f14c84b
slaughtbear <ivanaguirre368@gmail.com>
2024-08-02 20:27:15 -0600

    Se agregó servicio para db en tiempo real

--- airwise-systems/src/firebase/FirebaseConfig.jsx
+++ airwise-systems/src/firebase/FirebaseConfig.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'; // Inicialización de un app de Firebase
 import { getAuth } from "firebase/auth"; // Servicio de autenticación 
 import { getFirestore } from 'firebase/firestore'; // Servicio de Firestore
+import { getDatabase } from "firebase/database"; // Servicio de Base de Datos en tiempo real
 
 // Objeto con las credenciales de la aplicación
 const firebaseConfig = {
@@ -11,10 +12,12 @@ const firebaseConfig = {
   messagingSenderId: "640784192190",
   appId: "1:640784192190:web:1a0f89e08bcdc73f2d84ef",
   measurementId: "G-N4FB5W71S8",
+  databaseURL: "https://aws-db-e1c2d-default-rtdb.firebaseio.com/",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const database = getDatabase(app);
 
-export { app, auth, db };
+export { app, auth, db, database };
